38230da50e25387bcab0d14758060bd98e34a38f
Joao <joaovitorfvr@gmail.com>
2026-04-24 12:03:09 -0300

    merge

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
@@ -12,4 +13,20 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+=======
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlcKjGxpgomCi9frdPOtGPFqjAqZUykUg",
+  authDomain: "aula8-b3871.firebaseapp.com",
+  databaseURL: "https://aula8-b3871-default-rtdb.firebaseio.com",
+  projectId: "aula8-b3871",
+  storageBucket: "aula8-b3871.firebasestorage.app",
+  messagingSenderId: "220440658507",
+  appId: "1:220440658507:web:eb5a4feae2a73f335703ce"
+};
+
+const app = initializeApp(firebaseConfig);
+>>>>>>> Stashed changes
 export const db1 = getDatabase(app); // Exportando a instância do banco
